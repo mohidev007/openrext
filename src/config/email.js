dff7f2c42afad232739faee627c0c5fb697441c8
mohiuddin2 <mohibdrs@gmail.com>
2025-07-02 13:13:18 +0600

    fix: Correct Nodemailer method name from createTransporter to createTransport

--- src/config/email.js
+++ src/config/email.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
